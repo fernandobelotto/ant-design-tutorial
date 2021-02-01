131fd004c672720d7813af37480c5e11c4d7c574
Fernando Belotto <fernando.bbosco@gmail.com>
2021-02-01 00:01:56 -0300

    button file updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Button} from 'antd'
+import { SearchOutlined } from '@ant-design/icons';
 
 export default function App() {
   return (
     <>
       <h1>Ant Design Tutorial</h1>
+      <Button
+        icon={<SearchOutlined />}
+        htmlType="button" 
+        shape='circle'
+        size="large"
+        danger 
+        onClick={()=>console.log('testando')}>Um Botão</Button>
     </>
   )
 }
